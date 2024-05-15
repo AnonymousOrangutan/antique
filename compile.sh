a47a271de3fa03636efe53dad84148b980479945
RangTang <maximeleogateau@gmail.com>
2024-05-15 10:40:50 +0200

    smol changes, revisiting

--- compile.sh
+++ compile.sh
@@ -7,7 +7,7 @@ if [ -f "$file" ] ; then
 fi
 
 # Compile document
-pdflatex -interaction=nonstopmode -output-directory out main.tex
+xelatex -output-directory out main.tex
 
 # Once done, move PDF to working directory
 mv out/main.pdf main.pdf
